9f0dbd132eb4633cf22ece974f1c3b796c01e4bd
ejooss@chungbuk.ac.kr <ejooss@chungbuk.ac.kr>
2023-05-14 22:00:41 +0900

    OCR, OpenCV 테스트

---
+++ opencv/build/intermediates/cxx/Debug/336x3612/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\82107\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\82107\\Documents\\-BusinessCardApp\\opencv\\.cxx\\Debug\\336x3612\\x86_64" ^
+  opencv_jni_shared
